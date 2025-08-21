6ded19bf0950fbc6c2e77f031bdc3431adf1b93d
arsh-nav <46626841+arsh-nav@users.noreply.github.com>
2025-08-21 19:02:47 +0000

    sf

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get("/api/:date", (req, res) => {
   let date = /ˆ\d+$/.test(dateParam)
     ? new Date(parseInt(dateParam))
     : new Date(dateParam);
-  if (date.toString() === "Invalid Date") {
+  if (!isNaN(date) && date.toString() === "Invalid Date") {
     return res.json({ error: "Invalid Date" });
   }
   res.json({ unix: date.getTime(), utc: date.toUTCString() });
